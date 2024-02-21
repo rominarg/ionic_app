e99e137080bc16f8f53bf7dfc0a6615f51c52a44
RominaNavarrete <rominagnavarrete@gmail.com>
2024-02-21 19:39:08 -0300

    Navigating to different routes

--- src/app/customers/customers.page.ts
+++ src/app/customers/customers.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-customers',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomersPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  goToHome() {
+    this.router.navigate(['/home']);
+  }
 }
