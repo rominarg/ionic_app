b6a1da2f88c65fc3da00dd0da3e2fdc63d932c08
RominaNavarrete <rominagnavarrete@gmail.com>
2024-03-02 20:27:04 -0300

    categories

--- src/app/listing/listing.page.ts
+++ src/app/listing/listing.page.ts
@@ -7,35 +7,40 @@ import { Category } from '../models/category.models';
   styleUrls: ['./listing.page.scss'],
 })
 export class ListingPage implements OnInit {
-categories: Category[] = [] 
+  categories: Category[] = [];
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    // Llama al método getCategories al iniciar la página
+    this.getCategories();
+  }
 
-  getCategories () {
+  getCategories() {
+    // Asigna las categorías al array
     this.categories = [
       {
-        id:1,
-        label:'all',
+        id: 1,
+        label: 'all',
         image: 'asset/icon/all.png',
         active: false,
       },
       {
-        id:2,
-        label:'Burgers',
-        image: 
+        id: 2,
+        label: 'Burgers',
+        image: 'URL_DE_LA_IMAGEN', // Reemplaza con la URL de la imagen
         active: false,
       },
       {
-        id:3,
-        label:'dishes',
-        image: 
+        id: 3,
+        label: 'dishes',
+        image: 'URL_DE_LA_IMAGEN', // Reemplaza con la URL de la imagen
         active: false,
       },
       {
-        id:4,
-        label:'sushi',
-        image: 
+        id: 4,
+        label: 'sushi',
+        image: 'URL_DE_LA_IMAGEN', // Reemplaza con la URL de la imagen
         active: false,
       },
     ];
