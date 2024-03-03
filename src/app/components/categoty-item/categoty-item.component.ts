abbb35b906e060acdd2ab208854a4b6d354dd551
RominaNavarrete <rominagnavarrete@gmail.com>
2024-03-02 23:18:05 -0300

    categories

--- src/app/components/categoty-item/categoty-item.component.ts
+++ src/app/components/categoty-item/categoty-item.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Category } from 'src/app/models/category.models';
 
 @Component({
   selector: 'app-categoty-item',
   templateUrl: './categoty-item.component.html',
   styleUrls: ['./categoty-item.component.scss'],
 })
-export class CategotyItemComponent  implements OnInit {
-
-
-
+export class CategotyItemComponent {
+@Input() item: Category;
 }
